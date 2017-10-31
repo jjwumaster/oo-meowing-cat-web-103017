f11a584eb92b64122a870f0ac7a4ca586aa4fdaf
github username <github email address>
2017-10-31 17:53:49 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,24 @@
-## code your solution here. 
+## code your solution here.
+
+class Cat
+
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+
+
+  # def name=(name_input)
+  #   @name = name_input
+  # end
+  #
+  # def name
+  #   @name
+  # end
+  #
+  # def meow
+  #   puts "meow!"
+  # end
+
+end
